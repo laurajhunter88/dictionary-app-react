b8509efe11406f476dd7bc537b80257862d00d0a
laurajhunter88 <85752827+laurajhunter88@users.noreply.github.com>
2021-09-30 17:27:03 +0100

    added search form

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Dictionary.css";
 
 export default function Dictionary() {
-  return <div className="Dictionary"></div>;
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+
+  function keywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" autoFocus={true} onChange={keywordChange} />
+      </form>
+    </div>
+  );
 }
