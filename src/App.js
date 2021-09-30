31754bd8353173310a6dbff86d0d4d8cf96f8077
laurajhunter88 <85752827+laurajhunter88@users.noreply.github.com>
2021-09-30 17:11:39 +0100

    added structure to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <h1>Dictionary App</h1>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary App</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>Coded by Laura Hunter, hosted on Netlify</footer>
+      </div>
     </div>
   );
 }
