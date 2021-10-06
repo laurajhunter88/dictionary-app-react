4b70b50dd5690622784dbf755b4f6c25475a5019
laurajhunter88 <85752827+laurajhunter88@users.noreply.github.com>
2021-10-06 17:30:37 +0100

    added css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -29,7 +29,12 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" autoFocus={true} onChange={keywordChange} />
+        <input
+          className="search-bar"
+          type="search"
+          autoFocus={true}
+          onChange={keywordChange}
+        />
       </form>
       <Results results={results} />
     </div>
